84edb34d92b10e2f534191af7be970042d728a57
saciinol <darrenicolas18@gmail.com>
2024-07-19 13:55:07 +0800

    update 7/19/2024

---
+++ resources/js/components/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home(){
+    return <div>
+        <header class="bg-black flex justify-between mx-auto w-full px-[20px]">
+            <div class="flex">
+                <a href="#" class="py-[15px] px-[15px] bg-white">LOGO</a>
+            </div>
+
+            <div class="flex">
+                <a href="#" class="py-[15px] px-[15px] bg-blue-700">Home</a>
+                <a href="#" class="py-[15px] px-[15px] bg-red-700">About</a>
+                <a href="#" class="py-[15px] px-[15px] bg-green-700">Services</a>
+                <a href="#" class="py-[15px] px-[15px] bg-yellow-400">Contact</a>
+            </div>
+        </header>
+    </div>;
+}
---
+++ resources/js/components/app.jsx
@@ -0,0 +1,8 @@
+import '../bootstrap';
+
+import ReactDOM from 'react-dom/client';
+import Home from './Home';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Home />
+);
